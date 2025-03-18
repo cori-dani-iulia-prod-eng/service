405b852514bedf29a5007fd20d9671e09391f398
tcori <coriciuc.tudor@gmail.com>
2025-03-18 22:54:29 +0200

    [API] Modified StockMovement

--- src/main/java/ro/unibuc/hello/controller/StockMovementController.java
+++ src/main/java/ro/unibuc/hello/controller/StockMovementController.java
@@ -1,12 +1,13 @@
 package ro.unibuc.hello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import ro.unibuc.hello.dto.StockMovement;
+import ro.unibuc.hello.dto.CreateStockMovement;
 import ro.unibuc.hello.service.StockMovementService;
 import ro.unibuc.hello.exception.EntityNotFoundException;
 import ro.unibuc.hello.exception.InvalidInputException;
@@ -22,46 +23,46 @@ public class StockMovementController {
     private StockMovementService stockMovementService;
 
     @GetMapping
-    public List<StockMovement> getAllStockMovements() {
+    public List<CreateStockMovement> getAllStockMovements() {
         return stockMovementService.getAllStockMovements();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<StockMovement> getStockMovementById(@PathVariable String id) throws EntityNotFoundException {
-        StockMovement stockMovement = stockMovementService.getStockMovementById(id);
+    public ResponseEntity<CreateStockMovement> getStockMovementById(@PathVariable String id) throws EntityNotFoundException {
+        CreateStockMovement stockMovement = stockMovementService.getStockMovementById(id);
         return ResponseEntity.ok(stockMovement);
     }
 
     @PostMapping
-    public ResponseEntity<?> createStockMovement(@Valid @RequestBody StockMovement stockMovement, BindingResult result) throws InvalidInputException {
+    public ResponseEntity<?> createStockMovement(@Valid @RequestBody CreateStockMovement stockMovement, BindingResult result) throws InvalidInputException {
         if (result.hasErrors()) {
             String errorMessages = result.getAllErrors()
                     .stream()
-                    .map(error -> error.getDefaultMessage())
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
                     .reduce((message1, message2) -> message1 + ", " + message2)
                     .orElse("Invalid data");
             throw new InvalidInputException(errorMessages);
         }
-        StockMovement savedStockMovement = stockMovementService.saveStockMovement(stockMovement);
+        CreateStockMovement savedStockMovement = stockMovementService.saveStockMovement(stockMovement);
         return new ResponseEntity<>(savedStockMovement, HttpStatus.CREATED);
     }
 
     @PostMapping("/batch")
-    public List<StockMovement> saveAllStockMovements(@Valid @RequestBody List<StockMovement> stockMovements) throws InvalidInputException {
+    public List<CreateStockMovement> saveAllStockMovements(@Valid @RequestBody List<CreateStockMovement> stockMovements) throws InvalidInputException {
         return stockMovementService.saveAll(stockMovements);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateStockMovement(@PathVariable String id, @Valid @RequestBody StockMovement stockMovement, BindingResult result) throws EntityNotFoundException, InvalidInputException {
+    public ResponseEntity<?> updateStockMovement(@PathVariable String id, @Valid @RequestBody CreateStockMovement stockMovement, BindingResult result) throws EntityNotFoundException, InvalidInputException {
         if (result.hasErrors()) {
             String errorMessages = result.getAllErrors()
                     .stream()
-                    .map(error -> error.getDefaultMessage())
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
                     .reduce((message1, message2) -> message1 + ", " + message2)
                     .orElse("Invalid data");
             throw new InvalidInputException(errorMessages);
         }
-        StockMovement updatedStockMovement = stockMovementService.updateStockMovement(id, stockMovement);
+        CreateStockMovement updatedStockMovement = stockMovementService.updateStockMovement(id, stockMovement);
         return new ResponseEntity<>(updatedStockMovement, HttpStatus.OK);
     }
 
